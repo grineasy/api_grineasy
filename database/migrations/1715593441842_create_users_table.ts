4212c6855ab7a36cf1f9a698e886a098d0e9671c
Rémi <115152594+remimoul@users.noreply.github.com>
2024-05-14 22:59:10 +0200

    refactor: Update User model and migration
    - Add nullable constraint to company_id column in users table

--- database/migrations/1715593441842_create_users_table.ts
+++ database/migrations/1715593441842_create_users_table.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.integer('company_id').unsigned().references('id').inTable('companies').notNullable().defaultTo(1)
+      table.integer('company_id').unsigned().references('id').inTable('companies').nullable()
       table.string('firstName', 50).nullable()
       table.string('lastName', 50).nullable()
       table.string('email', 254).notNullable().unique()
