b5666c47669ea8d27088e702d64208b2204b2abd
Rémi <neptunes92390@outlook.fr>
2024-05-14 11:36:06 +0200

    Add login and logout functionality

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -1,5 +1,13 @@
 import vine from '@vinejs/vine'
 
+// nouveau validator
+export const loginValidator = vine.compile(
+  vine.object({
+    email: vine.string().email(),
+    password: vine.string().minLength(8).maxLength(32),
+  })
+)
+
 export const registerValidator = vine.compile(
   vine.object({
     firstName: vine.string().minLength(3).maxLength(64),
